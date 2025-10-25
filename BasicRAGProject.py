7a3206d128997d579b9282b6def9348c8bdb629a
Şevin Sönmez <sevinsonmezz@gmail.com>
2025-10-26 01:50:38 +0300

    Completed BasicRAGProject

--- BasicRAGProject.py
+++ BasicRAGProject.py
@@ -12,6 +12,9 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature = 0.1)
 
 loader = WebBaseLoader(
@@ -25,5 +28,36 @@ loader = WebBaseLoader(
 
 docs = loader.load()
 
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, #deneyerek bul
+    chunk_overlap=200 #(parça çakışması veya bindirmesi) bağlam kaybını önler.
+)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(
+    documents=splits,
+    embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+)
+
+#Bu kod satırı, pasif olan vectorstore (vektör deposu) nesnenizi, aktif olarak arama yapabilen bir retriever (getirici) nesnesine dönüştürür.
+retriever = vectorstore.as_retriever()
+
+#langchain hub rag prompt yaz google'a
+# hub'taki kullanıcıların hazırladığı promptları rahatlıkla hub.pull() ile çekebiliriz.
+prompt = hub.pull("rlm/rag-prompt")
+
+rag_chain = (
+    {"context": retriever | format_docs, "question":RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+
 if __name__ == '__main__':
-    print(docs)
+    #print(docs)
+    #print(format_docs(docs))
+
+    for chunk in rag_chain.stream(
+        "what is maximum inner product search?"
+    ):
+        print(chunk, end=" ",flush=True)
